ce9ee4825093dd66d0b7f0eb69a692011f24c34b
Mertalmali4 <mertalmali4@gmail.com>
2023-03-18 22:05:18 +0300

    Volume control with Hand Gestures features added

---
+++ HandDetection.py
@@ -0,0 +1,79 @@
+import cv2
+import mediapipe as mp
+from math import hypot
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+import numpy as np
+import sys
+
+cap = cv2.VideoCapture(0) 
+
+mpHands = mp.solutions.hands 
+hands = mpHands.Hands()  
+mpDraw = mp.solutions.drawing_utils
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+volbar = 400
+volper = 0
+volTemp=0
+volStop=0
+
+volMin, volMax = volume.GetVolumeRange()[:2]
+
+while True:
+    success, img = cap.read() 
+    if not success:
+        break
+    
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) 
+    results = hands.process(imgRGB) 
+
+    lmList = [] 
+    if results.multi_hand_landmarks: 
+        for handlandmark in results.multi_hand_landmarks:
+            for id, lm in enumerate(handlandmark.landmark): 
+                h, w, _ = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                lmList.append([id, cx, cy]) 
+            mpDraw.draw_landmarks(img, handlandmark, mpHands.HAND_CONNECTIONS)
+
+    if lmList:
+        x1, y1 = lmList[4][1], lmList[4][2] 
+        x2, y2 = lmList[8][1], lmList[8][2] 
+        cv2.circle(img, (x1, y1), 13, (255,0,0), cv2.FILLED) 
+        cv2.circle(img, (x2, y2), 13, (255,0,0), cv2.FILLED) 
+        cv2.line(img, (x1, y1), (x2, y2), (255,0,0), 4) 
+
+        length = hypot(x2-x1, y2-y1) 
+        vol = np.interp(length, [30, 350], [volMin, volMax]) 
+        volbar = np.interp(length, [30, 350], [400, 150])
+        volper = np.interp(length, [30, 350], [0, 100])
+
+        volume.SetMasterVolumeLevel(vol, None)
+        vol=int(vol)
+        
+        if( vol-1==volTemp or vol==volTemp or vol+1==volTemp):
+            if(volStop==30):
+                
+                sys.exit()
+            volStop+=1
+            volTemp=vol
+        else:
+            volTemp=vol
+            volStop=0
+
+        print(volStop)
+        volTemp=vol
+        cv2.rectangle(img, (50, 150), (85, 400), (0, 255, 0), 4)
+        cv2.rectangle(img, (50, int(volbar)), (85, 400), (0, 255, 0), cv2.FILLED)
+        cv2.putText(img, f"{int(volper)}%", (10, 40), cv2.FONT_ITALIC, 1, (10, 50, 0), 2)
+
+    cv2.imshow('Image', img) 
+    if cv2.waitKey(1) == ord(' '): 
+        break
+        
+cap.release()      
+cv2.destroyAllWindows() 
